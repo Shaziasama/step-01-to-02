e4ef68f0fce8cc4e992effd52119165310e8ceb9
Shaziasama <shaziasamma921@gmail.com>
2024-07-18 21:31:23 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+///step 00///
+let message = "Hello world!"; ///infering type, the variable name implict
+console.log(message);
+////step 002////
+//JSON stands for Javascript Object Notation.///
+////JSON is a text-based data format that is used to store and transfer data.////
+//// JSON syntax
+//{
+// "name": "Vipin",
+//"age": 21,
+//"gender": "male",
+//}
+// wait, Is JSON is similar to javaScript objects?
+//The Answer is No.
+//JavaScript objects can contain functions but JSON not.
+//JavaScript objects can only be used in JavaScript but JSON can be created and used by other programming languages.
+// JSON object
+const detail = { "name": "Vipin", "age": 21 };
+// accessing JSON object
+console.log(detail.name); // Vipin
+export {};
---
+++ main.ts
@@ -0,0 +1,64 @@
+///step 00///
+//let message = "Hello world!";///infering type, the variable name implict
+//console.log(message);
+
+////step 00a////
+//JSON stands for Javascript Object Notation.///
+////JSON is a text-based data format that is used to store and transfer data.////
+//// JSON syntax
+
+//{
+
+   // "name": "Vipin",
+   
+    //"age": 21,
+   
+    //"gender": "male",
+   //}
+   
+   // wait, Is JSON is similar to javaScript objects?
+   
+   //The Answer is No.
+   
+   //JavaScript objects can contain functions but JSON not.
+   //JavaScript objects can only be used in JavaScript but JSON can be created and used by other programming languages.
+   
+    // JSON object
+
+ //const detail = { "name": "Vipin", "age": 21 }
+
+ // accessing JSON object
+
+ //console.log(detail. name); // Vipin
+
+
+ //////step 00b////
+ //syntex error////
+ //lett message = "Hello World";//syntax error
+ //console.log(message);
+
+ ////step00c///
+ //type error//
+ //let message = "Hello World";
+//console.loger(message);
+
+
+////step 00d///
+///type error///
+//let message = "Hello World";
+//console.loger(message);
+
+
+
+
+ 
+
+  
+ 
+
+   
+   
+   
+
+
+
